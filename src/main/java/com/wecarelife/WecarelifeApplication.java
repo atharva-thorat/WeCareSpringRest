e2154339b3de408b90a7236fcd65f2c255248a07
atharva-thorat <41202172+T4ng021@users.noreply.github.com>
2023-09-10 20:19:21 +0530

    Add OpenDoc API for Swagger

--- src/main/java/com/wecarelife/WecarelifeApplication.java
+++ src/main/java/com/wecarelife/WecarelifeApplication.java
@@ -3,11 +3,10 @@ package com.wecarelife;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @SpringBootApplication
-//@EnableSwagger2
 public class WecarelifeApplication {
 
 	public static void main(String[] args) {
