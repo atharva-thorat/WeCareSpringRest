f967c5d0d74a7e8f3fb15cbfaec89019dd535e66
atharva-thorat <41202172+T4ng021@users.noreply.github.com>
2023-10-11 23:02:20 +0530

    changed Coach Booking Entity with Lombok Annotations

--- src/main/java/com/wecarelife/entity/Booking.java
+++ src/main/java/com/wecarelife/entity/Booking.java
@@ -6,8 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @GenericGenerator(name = "idgen", strategy = "increment")
 public class Booking {
@@ -20,49 +28,6 @@ public class Booking {
 		private LocalDate appointment_date; //Date of appointment
 		private String slot;				//Time slot of appointment
 		static Integer counter = 101;
-		
-		public Booking() {}
-		
-		public Booking(Integer booking_id,String user_id,String coach_id,LocalDate appointment_date,String slot) {
-			
-			this.booking_id = booking_id;
-			this.user_id = user_id;
-			this.coach_id = coach_id;
-			this.appointment_date = appointment_date;
-			this.slot = slot;
-		}
-		
-		public Integer getBooking_id() {
-			return booking_id;
-		}
-		public void setBooking_id(Integer booking_id) {
-			this.booking_id = booking_id;
-		}
-		public String getUser_id() {
-			return user_id;
-		}
-		public void setUser_id(String user_id) {
-			this.user_id = user_id;
-		}
-		public String getCoach_id() {
-			return coach_id;
-		}
-		public void setCoach_id(String coach_id) {
-			this.coach_id = coach_id;
-		}
-		public LocalDate getAppointment_date() {
-			return appointment_date;
-		}
-		public void setAppointment_date(LocalDate appointment_date) {
-			this.appointment_date = appointment_date;
-		}
-		public String getSlot() {
-			return slot;
-		}
-		public void setSlot(String slot) {
-			this.slot = slot;
-		}
-		
-		
+
 }
 
--- src/main/java/com/wecarelife/entity/Coach.java
+++ src/main/java/com/wecarelife/entity/Coach.java
@@ -7,11 +7,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 //import javax.validation.Valid;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.validation.annotation.Validated;
 
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @GenericGenerator(name = "idgen", strategy = "increment")
 public class Coach {
@@ -25,62 +32,5 @@ public class Coach {
 		private String password;
 		private Long mobile_number;
 		private String speciality;
-		
-		public Coach() {}
-		
-		public Coach (String name,String gender,LocalDate date_of_birth,
-				String password,Long mobile_number,String speciality) {
-			
-			this.name = name;
-			this.gender = gender;
-			this.date_of_birth = date_of_birth;
-			this.password = password;
-			this.mobile_number = mobile_number;
-			this.speciality = speciality;
-		}
-		
-		
-		public Integer getCoach_id() {
-			return coach_id;
-		}
-		public void setCoach_id(Integer coach_id) {
-			this.coach_id = coach_id;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getGender() {
-			return gender;
-		}
-		public void setGender(String gender) {
-			this.gender = gender;
-		}
-		public LocalDate getDate_of_birth() {
-			return date_of_birth;
-		}
-		public void setDate_of_birth(LocalDate date_of_birth) {
-			this.date_of_birth = date_of_birth;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
-		public Long getMobile_number() {
-			return mobile_number;
-		}
-		public void setMobile_number(Long mobile_number) {
-			this.mobile_number = mobile_number;
-		}
-		public String getSpeciality() {
-			return speciality;
-		}
-		public void setSpeciality(String speciality) {
-			this.speciality = speciality;
-		}
 		
 }
